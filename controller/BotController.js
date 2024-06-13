854bb5ab6448712b73051503445c0211ccf1378c
Bebby Ilham Akbar Aresta <bebbyilham@gmail.com>
2024-06-13 11:36:54 +0700

    first commit

---
+++ controller/BotController.js
@@ -0,0 +1,20 @@
+const { Controller, Response } = require("pepesan");
+const f = require("../utils/Formatter");
+
+module.exports = class BotController extends Controller {
+  async introduction(request) {
+    return Response.menu.fromArrayOfString(
+      [f("menu.daftarProduk"), f("menu.alamatKantor")],
+      f("intro", [request.name]),
+      f("template.menu")
+    );
+  }
+
+  async product(request) {
+    return this.reply("Ini produk");
+  }
+
+  async alamatKantor(request) {
+    return this.reply("Alamat kantor kami ada di Jakarta");
+  }
+};
